ab17f5c3cfda53619987c17493488492ebf623c1
pranith-singh <thakurpranithraj@gmail.com>
2025-11-24 15:28:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+// Serve static files from public
+app.use(express.static(path.join(__dirname, "public")));
 
+// API route
 app.get("/api/hello", (req, res) => {
   res.json({ message: "Hello from Node.js Web App!" });
 });
 
-app.get("/", (req, res) => {
+// Serve index.html for all other routes
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
